3d71483578ca7e9b158da9c4a2d67de09b1e71c2
hadeer <hadeer.abelgawad44@gmail.com>
2025-05-20 16:42:24 +0300

    'sendemail'

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -42,16 +42,21 @@ function Contact() {
    })
 
    async function handleSendOrder(formsData, { resetForm }){
-    await axios.post('http://localhost:3000/api/order/api/order', formsData)
+    setIsSubmitting(true);
+    await axios.post('http://localhost:3000/api/order', formsData)
     .then((response)=>{
         console.log(response);
         toast.success('تم إرسال طلبك بنجاح')
-        resetForm()
+        formikHelpers.resetForm();
+        setIsSubmitting(false)
+        
 
     }).catch((error)=>{
         console.error('خطأ أثناء الإرسال:', error)
         toast.error('حدث خطأ أثناء الإرسال')
+        setIsSubmitting(false)
     }
+    
     )
    }
 
@@ -149,14 +154,14 @@ function Contact() {
                 <p className="text-red-500 text-sm">{formik.errors.details}</p>
               )}
 
-
-              <button
-                type="submit"
-                className="bg-[#FFCA00] w-[90%]  md:w-[55%] py-3 rounded-lg hover:cursor-pointer"
-                 disabled={isSubmitting} onClick={() => setIsSubmitting(true)} >
-              {isSubmitting ? 'جاري الإرسال…' : 
-              '  - وإترك رقم الهاتف والخدمة المراد تنظيفها وسوف نقوم بالتواصل معك مباشرةإضغط هنا…'}
+            <button
+              type="submit"
+              className="bg-[#FFCA00] w-[90%] md:w-[55%] py-3 rounded-lg hover:cursor-pointer"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? '…جاري الإرسال' : '  - وإترك رقم الهاتف والخدمة المراد تنظيفها وسوف نقوم بالتواصل معك مباشرةإضغط هنا…'}
             </button>
+
             </form>
           </div>
         </div>
